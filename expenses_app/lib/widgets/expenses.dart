eda1a2757b4565287fbcd04f3dd71aeb7ba6ff72
hardardottir93 <hafrunhardar@gmail.com>
2025-04-24 22:58:05 +0000

    section 6

--- expenses_app/lib/widgets/expenses.dart
+++ expenses_app/lib/widgets/expenses.dart
@@ -32,6 +32,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay(){
     showModalBottomSheet(
+      useSafeArea: true, // heldur öllu frá myndavélinni t.d.
       isScrollControlled: true, //passar að lyklaborð fari ekki yfir glugga
       context: context,
       // => Text(... er eins og að gera fall og return
@@ -69,6 +70,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -90,9 +93,18 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body: width < 600
+        ? Column(
+          children: [
+            Chart(expenses: _registeredExpenses),
+            Expanded(
+              child: mainContent,
+            ),
+          ],
+      ) : Row(
         children: [
-          Chart(expenses: _registeredExpenses),
+          Expanded(
+            child: Chart(expenses: _registeredExpenses)),
           Expanded(
             child: mainContent,
           ),
